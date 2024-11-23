1e450ebe93bac3732baefcab6271164ce9d57402
Alok Menghrajani <441307+alokmenghrajani@users.noreply.github.com>
2024-11-23 11:03:30 +0100

    plan of action

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("A Network Stack From Scratch, enjoy! 🎉")
+
+	// Step 1: send and receive ethernet frames
+	// Step 2: send and receive IPv4 datagrams
+	// Step 3: send and receive UDP packets
+	// Step 4: get an IPv4 address using DHCPv4
+	// Step 5: resolve xkcd.com's IPv4 address using DNS
+	// Step 6: send and receive TCP packets
+	// Step 7: send and receive HTTP requests
+	// Step 8: TLS handshake
+	// Step 9: send and receive HTTPS requests
+	// Step 10: fetch https://xkcd.com/atom.xml and display today's comic
+}
